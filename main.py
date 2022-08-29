da5482474c214adab2eac75156157e0e3a00c011
Simon.Hauri <simon.hauri@ii-vi.com>
2022-08-29 13:53:15 +0200

    changed port

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 from tfmini import TFmini
 
 # create the sensor and give it a port and (optional) operating mode
-tf = TFmini('/dev/tty.usbserial-A506BOT5', mode=TFmini.STD_MODE)
+tf = TFmini('/dev/ttyAMA0', mode=TFmini.STD_MODE)
 
 try:
     print('='*25)
