287e7d092db60779ba17d29f593ebf16993b7bb8
Simon.Hauri <simon.hauri@ii-vi.com>
2022-08-29 13:51:55 +0200

    new library

--- main.py
+++ main.py
@@ -1,27 +1,19 @@
-# -*- coding: utf-8 -*
-import serial
+import time
+from tfmini import TFmini
 
-ser = serial.Serial("/dev/ttyAMA0", 115200)
+# create the sensor and give it a port and (optional) operating mode
+tf = TFmini('/dev/tty.usbserial-A506BOT5', mode=TFmini.STD_MODE)
 
-
-def getTFminiData():
+try:
+    print('='*25)
     while True:
-        count = ser.in_waiting
-        if count > 8:
-            recv = ser.read(9)
-            ser.reset_input_buffer()
-            if recv[0] == 'Y' and recv[1] == 'Y':  # 0x59 is 'Y'
-                low = int(recv[2].encode('hex'), 16)
-                high = int(recv[3].encode('hex'), 16)
-                distance = low + high * 256
-                print(distance)
+        d = tf.read()
+        if d:
+            print(f'Distance: {d:5}')
+        else:
+            print('No valid response')
+        time.sleep(0.1)
 
-
-if __name__ == '__main__':
-    try:
-        if ser.is_open == False:
-            ser.open()
-        getTFminiData()
-    except KeyboardInterrupt:  # Ctrl+C
-        if ser != None:
-            ser.close()
+except KeyboardInterrupt:
+    tf.close()
+    print('bye!!')
